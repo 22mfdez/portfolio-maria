79153d05cca01b4326ac29307f72ea23ad29b250
usuario1 <usuario@gmail.com>
2024-02-05 13:02:23 +0100

    Add @iconify/tailwind package and update package.json and package-lock.json

--- tailwind.config.js
+++ tailwind.config.js
@@ -99,5 +99,6 @@ module.exports = {
     require("@tailwindcss/typography"),
     require("@tailwindcss/forms"),
     require("@tailwindcss/aspect-ratio"),
+    require("@iconify/tailwind")
   ],
 };
